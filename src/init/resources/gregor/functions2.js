740a04be71690f1f96d23febc9378a600a6ba7dd
gregmavr <gregmavr@users.noreply.github.com>
2019-06-02 20:13:18 +0300

    add documentation

--- src/init/resources/gregor/functions2.js
+++ src/init/resources/gregor/functions2.js
@@ -15,3 +15,16 @@ $(function greg1(a,b){
 $(function greg3(){
     var Todo = Backbone.Model.extend();
 })(a,b,c,d);
+
+(function($) {
+    // The $ is now a local parameter and you can use it
+    // without worrying if another library overrode window.$
+}(window.jQuery));
+
+(function(global) {
+    // Capturing the Global Object
+  })(this);
+
+  (function(w, d, u) {
+    // Optimization for Minification
+  })(window, document);
