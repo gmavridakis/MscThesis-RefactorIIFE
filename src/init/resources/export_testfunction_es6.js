f3d7ee91fb13593af1bad1af2f5276a2b060e8f7
gregmavr <gregmavr@users.noreply.github.com>
2019-06-30 19:29:17 +0300

    upload latest changes

---
+++ src/init/resources/export_testfunction_es6.js
@@ -0,0 +1,23 @@
+/*
+    Here is exported only the body of each function within IIFE
+    Each IIFE is a different export file
+    
+    Init code :
+
+    var obj = (function () {
+        let obj = {
+            "name" : "greg"
+        }
+        return obj;
+    })();
+
+*/
+
+export function refactored_es6_1() {
+    return function (){
+        let obj = {
+            "name" : "greg"
+        }
+        return obj;
+    }
+}
